37723cf3c9a59dc7d37c85594602d07693e80581
AccountFromPL <beniaminkowol@gmail.com>
2024-03-23 08:55:37 +0100

    Create pretty-dump.js

---
+++ pretty-dump.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    let frames = document.getElementsByTagName("pre");
+    let indentGuide = '<div class="indent-guide"></div>';
+    let cssId = 'pretty-dump-css';
+    if (!document.getElementById(cssId)) {
+        let head = document.getElementsByTagName('head')[0];
+        let link = document.createElement('link');
+        link.id = cssId;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'http://website.example/css/stylesheet.css';
+        link.media = 'all';
+        head.appendChild(link);
+    }
+    Object.keys(frames).forEach(function (frame) {
+        let code = frames[frame].innerHTML.split("\n");
+        frames[frame].innerHTML = "";
+        Object.keys(code).forEach(function (_code) {
+            let span = document.createElement("span");
+            if (code[_code] !== "" && _code != code.length) {
+                let codeInsert = code[_code];
+                if(codeInsert.match(/^ {4,}/)) {
+                    let countSpaces = codeInsert.search(/\S/);
+                    if(0 === countSpaces % 2) {
+                        codeInsert = codeInsert.replace(/^ {4}/gm, `  ${indentGuide}  `);
+                        codeInsert = codeInsert.replace(/[ |>] {4}/g, `  ${indentGuide}  `);
+                    }
+                }
+                span.innerHTML = codeInsert;
+                frames[frame].appendChild(span);
+            }
+        });
+    });
+}
